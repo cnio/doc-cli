77649bfdc33c3d9363e4bfe0602ea407a58d65de
joes <hi@wem.me>
2016-04-20 16:26:07 +0800

    Add colorful

---
+++ tests/test.js
@@ -0,0 +1,15 @@
+var color = require('colorful')
+var hi = color.white('hello')
+//var i = color.underline('hello')
+//color.red(color.underline('hello'))
+//
+console.log(hi)
+//console.log(i)
+
+
+//var Color = require('colorful').Color;
+//
+//var s = new Color('colorful');
+// s.fgcolor = 13;
+//s.bgcolor = 214;
+//console.log(s)
