22b210103667c6fca1c6eaa9071dd2e1ae0c2fdd
joes <hi@wem.me>
2016-04-21 19:26:39 +0800

    Modified prompt

--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,19 @@
-var color = require('colorful')
-var hi = color.white('hello')
-//var i = color.underline('hello')
-//color.red(color.underline('hello'))
-//
-console.log(hi)
-//console.log(i)
+var colors = require('colors');
+colors.setTheme({
+    silly: 'rainbow',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+});
 
-
-//var Color = require('colorful').Color;
-//
-//var s = new Color('colorful');
-// s.fgcolor = 13;
-//s.bgcolor = 214;
-//console.log(s)
+console.log('hello'.debug); // outputs green text
+//console.log('i like cake and pies'.underline.red) // outputs red underlined text
+//console.log('inverse the color'.inverse); // inverses the color
+//console.log('OMG Rainbows!'.rainbow); // rainbow
+//console.log('Run the trap'.trap); // Drops the bass
